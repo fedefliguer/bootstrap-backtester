d876aa3ec00c6d12f73bc96b210e894cc6912564
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2022-12-30 20:10:22 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from bootstrap-backtester import __version__
+
+setup(
+    name='bootstrap_backtester',
+    version=__version__,
+
+    url='https://github.com/fedefliguer/bootstrap_backtester',
+    author='Federico Fliguer',
+    author_email='federico.fliguer@gmail.com',
+
+    py_modules=['bootstrap_backtester'],
+)
